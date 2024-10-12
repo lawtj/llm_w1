5bcda377b72d6dbae886278baed6f9be71e4e983
lawtj <41483192+lawtj@users.noreply.github.com>
2024-10-12 10:30:26 -0700

    new golden source questsions in data_sources.py

--- data_sources.py
+++ data_sources.py
@@ -1,15 +1,5 @@
 from langchain_community.document_loaders import TextLoader, JSONLoader, DirectoryLoader
 
-# If true, use smaller ttm_data set (5 custom papers). Otherwise use full scraped data
-GOLDEN_SOURCE = False
-if GOLDEN_SOURCE:
-    data_folder = "ttm_data"
-    loader = TextLoader(file_path=data_folder + "/scraped_markdown.md")
-else:
-    data_folder = "data_by_file"
-    loader = DirectoryLoader(data_folder, glob='**/*.json', show_progress=False, loader_cls=JSONLoader, loader_kwargs = {'jq_schema':'.full_content', 'text_content':False})
-
-LANGCHAIN_DATA = loader.load()
 
 # Full TTM papers for feeding into system prompt for golden reference q&a:
 with open("ttm_data/scraped_markdown.md", "r") as f:
@@ -22,3 +12,26 @@ with open("data/steroid_summaries.md", "r") as f:
 # Dialysis summaries for feeding into system prompt for golden reference q&a:
 with open("data/dialysis_summaries.md", "r") as f:
     DIALYSIS_SUMMARIES = f.read()
+
+GOLDEN_QUESTIONS = {
+    'TTM':[
+        "What is the optimal duration of TTM?",
+        "What is the optimal temperature for TTM?",
+        "What are the current controversies in the evidence for use of TTM?",
+        "When should TTM be initiated?",
+        "What is the indication for initiation of TTM?"
+    ],
+    'STEROID':[
+        "What is an evidence based dose of steroids for sepsis?",
+        "For what conditions is there evidence for the use of steroids in the ICU?",
+        "What are the indications for the use of steroids in the ICU?",
+        "What is the evidence for the use of steroids in respiratory conditions?",
+        "When should steroids be initiated in a patient with sepsis?",
+    ],
+    'DIALYSIS':[
+        "When should dialysis be initiated in a patient with acute kidney injury?",
+        "Is early or late initiation of dialysis associated with better outcomes in the ICU?",
+        "What are the current controversies in the use of dialysis in the ICU?",
+        "What are the indications for the use of dialysis in the ICU?",
+    ]
+}
